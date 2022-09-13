2af341913a34e3a08961760b0a4273edf2978798
Amit <amitbaghel690@gmail.com>
2022-09-13 17:32:22 +0530

    prime number and pallindrome function

---
+++ index.js
@@ -0,0 +1,26 @@
+// Prime Problem and  [ will intentionally write the incorrect code ]
+
+let num=13;
+let count=0;
+for(let i=0;i<=num;i++){
+   if(num%i!=0){
+   count++;
+}
+if(count==2){
+console.log("yes")
+}
+else{
+console.log("no")
+}
+}
+//Palindrome problem
+
+let str="naman";
+let str2="";
+for(let i=str.length-1;i>=0;i++){
+    str1+=str[i];
+}if(str1==str){
+console.log("yes)
+}else{
+console.log("no")
+}
